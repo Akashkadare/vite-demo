67c8b2c71f1b3e8abb6bb8b89c7c5da014090817
Akashkadare <akashkadare30@gmail.com>
2024-08-17 16:46:47 +0530

    first commit

--- src/usememo/Childmemo.jsx
+++ src/usememo/Childmemo.jsx
@@ -4,6 +4,7 @@ const Childmemo = (props) => {
     console.log("hello child component")
   return (
     <div>
+      <h1>Child demo</h1>
        <h1 style={{backgroundColor:"blue"}}>childcomponet {props.adds}</h1>
     </div>
   )
